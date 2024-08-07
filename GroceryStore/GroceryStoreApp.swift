f8b5bb94c2cc0e1f53d64719d5d5ff6d4c01225e
Kamil <biktineyev@hotmail.com>
2024-08-07 16:05:11 +0300

    Initial Commit

---
+++ GroceryStore/GroceryStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreApp.swift
+//  GroceryStore
+//
+//  Created by Kamil Biktineyev on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
